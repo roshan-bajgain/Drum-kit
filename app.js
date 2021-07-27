cc2e5e427afb585c8d56facf8f971f71fc7a1344
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-07-27 13:53:02 +0545

    Did js upto line 8

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 window.addEventListener("keydown", function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-    if(!audio) return;
+    const audio = document.querySelector(`.key[data-key="${e.keycode}"]`);
+    if (!audio) return;
+    audio.CurrentTime = 0;
     audio.play();
+    console.log(key);
 });
