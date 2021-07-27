d7bbb4eba02b0baaeddebac7b1ba8f83fc02a33a
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-07-27 14:34:03 +0545

    add key code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 window.addEventListener("keydown", function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
+    console.log(e.key);
     if (!audio) return;
     audio.CurrentTime = 0;
     audio.play();
-    console.log(key);
 });
