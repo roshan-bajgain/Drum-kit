2dc0b40551ad71c0a67b24a7ff0afc12f2136f8a
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-07-27 13:54:44 +0545

    Did css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-    const audio = document.querySelector(`.key[data-key="${e.keycode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
     if (!audio) return;
     audio.CurrentTime = 0;
     audio.play();
