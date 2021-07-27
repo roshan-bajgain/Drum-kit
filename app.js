20591e4d2707884ba814c4fd7e621759e2aa4885
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-07-27 14:09:07 +0545

    Add keycode

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.CurrentTime = 0;
     audio.play();
